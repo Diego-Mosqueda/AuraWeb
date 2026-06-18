6e6c4504fcd49c0f38af044c29c03cf07fda6bf8
Diego Mosqueda <dm7453681@gmail.com>
2026-06-18 15:38:22 -0600

    fix: base config para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -17,13 +17,13 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/AuraWeb/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
-    base: '/AuraWeb/',
   ],
   resolve: {
     alias: {
